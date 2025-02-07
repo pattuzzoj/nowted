81487fd753081164fb6c538298d43011c39491e4
pattuzzoj <pattuzzo@protonmail.com>
2025-02-07 11:05:37 +0000

    fix: update import paths in app.module.ts to use relative paths

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,8 +1,8 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
-import { AuthModule } from '@modules/auth/auth.module';
-import { BlockNonBrowser } from '@shared/middlewares/blockNonBrowser.middleware';
-import { SyncModule } from '@modules/sync/sync.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { BlockNonBrowser } from './shared/middlewares/blockNonBrowser.middleware';
+import { SyncModule } from './modules/sync/sync.module';
 
 @Module({
   imports: [
