26df31f4a3739e508d71f4706441db3a43861407
pattuzzoj <pattuzzo@protonmail.com>
2025-02-09 07:46:11 +0000

    change module-alias from package.json to main.js

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,4 +1,15 @@
-import 'module-alias/register';
+import moduleAlias from 'module-alias';
+
+console.log(__dirname);
+
+moduleAlias.addAliases({
+  "@root": __dirname,
+  "@database": __dirname.concat("src/database"),
+  "@modules": __dirname.concat("src/modules"),
+  "@shared": __dirname.concat("src/shared"),
+  "@utils": __dirname.concat("src/utils"),
+});
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 const cookieParser = require('cookie-parser');
