6627083df52ab677baaad94f384ffa8e12edea82
pattuzzoj <pattuzzo@protonmail.com>
2025-04-12 08:32:22 +0000

    (client): fix request api

--- client/src/features/profile/services/userService.ts
+++ client/src/features/profile/services/userService.ts
@@ -2,8 +2,8 @@ import { messages } from "@/shared/utils/messages";
 import { Notify } from "@/shared/utils/decorators/notify";
 import UserValidationService from "@/shared/services/userValidationService";
 import { SetStoreFunction } from "solid-js/store";
-import axios from "axios";
 import { ResponseDataType } from "@/shared/types";
+import api from "@/shared/services/api";
 
 type ProfileType = {
   username: string;
@@ -42,13 +42,13 @@ export default class UserService {
   }
 
   public async getProfile() {
-    const response = await axios.get<ResponseDataType>(this.baseURL + "/me");
+    const response = await api.get<ResponseDataType>(this.baseURL + "/me");
     return response.data;
   }
 
   @Notify(messages.USERNAME_CHANGE)
   public async changeUsername(username: string, password: string) {
-    await axios.patch<ResponseDataType>(this.baseURL + "/me/username", {
+    await api.patch<ResponseDataType>(this.baseURL + "/me/username", {
       username,
       password,
     });
@@ -56,7 +56,7 @@ export default class UserService {
 
   @Notify(messages.PASSWORD_CHANGE)
   public async changePassword(currentPassword: string, newPassword: string) {
-    await axios.patch<ResponseDataType>(this.baseURL + "/me/password", {
+    await api.patch<ResponseDataType>(this.baseURL + "/me/password", {
       currentPassword,
       newPassword,
     });
@@ -64,7 +64,7 @@ export default class UserService {
 
   @Notify(messages.EMAIL_CHANGE_REQUEST)
   public async requestChangeEmail(newEmail: string, password: string) {
-    await axios.post<ResponseDataType>(this.baseURL + "/request-change-email", {
+    await api.post<ResponseDataType>(this.baseURL + "/request-change-email", {
       newEmail,
       password,
     });
@@ -72,18 +72,18 @@ export default class UserService {
 
   @Notify(messages.EMAIL_CHANGE_CONFIRM)
   public async confirmChangeEmail(pin: number) {
-    await axios.post<ResponseDataType>(this.baseURL + "/confirm-change-email", {
+    await api.post<ResponseDataType>(this.baseURL + "/confirm-change-email", {
       pin,
     });
   }
 
   @Notify(messages.DELETING_DATA)
   public async deleteData() {
-    await axios.delete<ResponseDataType>(this.baseURL + "/delete-data");
+    await api.delete<ResponseDataType>(this.baseURL + "/delete-data");
   }
 
   @Notify(messages.DELETING_ACCOUNT)
   public async deleteAccount() {
-    await axios.delete<ResponseDataType>(this.baseURL);
+    await api.delete<ResponseDataType>(this.baseURL);
   }
 }
